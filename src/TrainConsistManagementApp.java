d690d3fcda822980f731a957ef8f045e16c1030c
Hannahimtiaz <hannahkhan680@gmail.com>
2026-03-30 14:08:37 +0530

    UC1: Add welcome message module

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,3 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class TrainConsistManagementApp {
 
+    public static void main(String[] args) {
+
+        // Display Welcome banner
+        System.out.println("====================================");
+        System.out.println("     Train Consist Management App   ");
+        System.out.println("====================================\n");
+
+        // Create a dynamic list to store train bogies
+        List<String> trainConsist = new ArrayList<>();
+
+        // Display initial consist information
+        System.out.println("Initial number of bogies: " + trainConsist.size());
+
+        // Print current state of the train
+        System.out.println("Current train consist: " + trainConsist);
+    }
 }
